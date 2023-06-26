dcc190012a6c4611b13cc78041799c051171b9a0
Dino Rodriguez <dinoorodriguez@gmail.com>
2023-06-26 16:54:51 -0400

    snippet

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -22,6 +22,8 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
